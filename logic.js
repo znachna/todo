98e28417389547870a42e535146082f67f410c25
znachna <znachnamail@gmail.com>
2021-11-13 19:05:20 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,86 +1,128 @@
-class Task {
-    constructor() {
-        this.taskDiv = document.createElement(`div`);
-        this.taskDiv.className = `task`;
-        this.taskDiv.tabIndex = `0`;
-
-        this.isCompleted = function() {
-            const checkbox = this.taskDiv.firstChild;
-            return checkbox.checked;
-        };
-
-        this.changeClassName = function() {
-            const taskIsCompleted = this.isCompleted();
-            if (taskIsCompleted) {
-                this.taskDiv.className = `task completed`;
-            } else {
-                this.taskDiv.className = `task`;
-            }
+const deleteTaskFromLocalStorage = (index) => {
+    if (index + 1 !== localStorage.length) {
+        for (let i = index + 1; i  < localStorage.length; i++) {
+            const temporalTask = localStorage.getItem(i);
+            localStorage.removeItem(i - 1);
+            localStorage.setItem(i - 1, temporalTask);
         }
 
-        this.getTextValue = function() {
-            const textbox = this.taskDiv.childNodes[1];
-            return textbox.innerText;
+        index = localStorage.length - 1;
+    }
+    localStorage.removeItem(index);
+}
+
+const addTaskToLocalStorage = (task, index = localStorage.length) => {
+    const localStorageTask = {
+        completed: task.isCompleted(),
+        textValue: task.getTextValue(),
+    };
+
+    if (index !== localStorage.length) {    
+        for (let i = localStorage.length; i > index; i--) {
+            const temporalTask = localStorage.getItem(i - 1);
+
+            localStorage.removeItem(i - 1);
+            localStorage.setItem(i, temporalTask);
+        }
+    }
+    localStorage.setItem(index, JSON.stringify(localStorageTask));
+}
+
+const getLocalStorage = (taskHandler) => {
+    const fromLocalStorage = true; 
+    
+    if (localStorage.length) {
+        for (let i = 0; i < localStorage.length; i++) {
+            const localStorageTask = JSON.parse ( localStorage.getItem(i) );
+
+            taskHandler.handleNewTask(localStorageTask.textValue, localStorageTask.completed, fromLocalStorage);
         }
     }
 }
 
+class Task {
+    constructor() {
+        this.taskDiv = document.createElement('div');
+        this.taskDiv.className = 'task';
+        this.taskDiv.tabIndex = 0;
+        this.isOnFocus = false;
+
+        this.taskDiv.addEventListener("focus", 
+            () => {
+                this.isOnFocus = true;
+            }
+        );
+
+        this.taskDiv.addEventListener("blur",
+            () => {
+                this.isOnFocus = false;
+            }
+        );
+
+        this.isCompleted = () => this.taskDiv.firstChild.checked;
+
+        this.changeClassName = () => this.isCompleted() ? this.taskDiv.className = 'task completed' : this.taskDiv.className = 'task';
+
+        this.getTextValue = () => this.taskDiv.childNodes[1].innerText;
+    }
+}
+
 class CheckBox {
-    constructor (taskHandler, isCompleted) {
-        this.element = document.createElement(`input`);
-        this.element.type = `checkbox`;
+    constructor (taskHandler, isCompleted = false) {
+        this.element = document.createElement('input');
+        this.element.type = 'checkbox';
         this.element.checked = isCompleted;
         this.taskHandler = taskHandler;
 
-        this.element.addEventListener(`click`,
+        this.element.addEventListener('click',
             (event) => {
                 const task = event.target.parentNode;
-                let taskIndex =  this.taskHandler.allTasks.findIndex(
-                     (taskFromArray) => taskFromArray.taskDiv === event.target.parentNode
+                const taskIndex =  this.taskHandler.allTasks.findIndex(
+                     (taskFromArray) => taskFromArray.taskDiv === task
                 );
                 const taskObject = this.taskHandler.allTasks[taskIndex];
+
+                deleteTaskFromLocalStorage(taskIndex);
+                this.taskHandler.allTasks.splice(taskIndex, 1);
+
                 if (event.target.checked) {
-                    this.taskHandler.allTasks.push(taskObject);
-                    task.parentNode.append(task);
+                    this.taskHandler.addNewTask(taskObject);
                 } else {
                     this.taskHandler.addToUncompletedTasks(taskObject);
-                    taskIndex++;
                 }
+
                 taskObject.changeClassName();
-                this.taskHandler.activeTaskIndex = -1;
-                this.taskHandler.allTasks.splice(taskIndex, 1);
-                setLocalStoradge(this.taskHandler.allTasks);
             }
         );
     }
 }
 
 class TextBox {
-    constructor (someText) {
-        this.textBoxDiv = document.createElement(`div`);
-        this.textBoxDiv.className = `taskText`;
-        this.textBoxDiv.innerText = someText; 
+    constructor (taskText) {
+        this.textBoxDiv = document.createElement('div');
+        this.textBoxDiv.className = 'taskText';
+        this.textBoxDiv.innerText = taskText; 
     }
 }
 
 class TrashButton {
     constructor (taskHandler) {
-        this.element = document.createElement(`input`);
-        this.element.type = `button`;
-        this.element.className = `trash`; 
+        this.element = document.createElement('input');
+        this.element.type = 'button';
+        this.element.className = 'trash'; 
         this.taskHandler = taskHandler;
 
-        this.element.addEventListener(`click`,
+        this.element.addEventListener('click',
             (e) => {
                 const task = e.target.parentNode;
                 const taskIndex = this.taskHandler.allTasks.findIndex(
                     (arrayTask) => arrayTask.taskDiv === task
                 );
-                this.taskHandler.activeTaskIndex = -1;
+
                 this.taskHandler.allTasks.splice(taskIndex, 1);
                 task.parentNode.removeChild(task);
 
-                setLocalStoradge(this.taskHandler.allTasks); 
+                deleteTaskFromLocalStorage(taskIndex);
             }
         );
     }
@@ -92,11 +134,18 @@ function TaskHandler(texterId, adderId) {
 
         adder: document.getElementById(adderId),
         
-        allTasks: new Array(),
+        allTasks: [],
 
-        activeTaskIndex: -1,
+        addNewTask(task, fromLocalStorage = false, node = document.body) {
+            this.allTasks.push(task);
+            node.append(task.taskDiv);
 
-        createNewTask(textValue, isCompleted=false) {
+            if (!fromLocalStorage) {
+                addTaskToLocalStorage(task);
+            }
+        },
+
+        createNewTask(textValue, isCompleted) {
             const task = new Task();
             const checkbox = new CheckBox(this, isCompleted);
             const textBox = new TextBox(textValue);
@@ -105,164 +154,121 @@ function TaskHandler(texterId, adderId) {
             task.taskDiv.append(checkbox.element);
             task.taskDiv.append(textBox.textBoxDiv);
             task.taskDiv.append(trashButton.element);
+            task.changeClassName();
+
             return task;
         },
 
-        readLocalStoradge(textValue, isCompleted=false) {
-            const task = this.createNewTask(textValue, isCompleted);
-            document.body.append(task.taskDiv);
-            task.changeClassName();
-            this.allTasks.push(task);
-        },
-
-        addNewTask(textValue, isCompleted=false) {
-            const task = this.createNewTask(textValue, isCompleted);
-            this.addToUncompletedTasks(task);
-
-            setLocalStoradge(this.allTasks);
-        },
-        handleNewTask() {
-            if (this.texter.value) {
-                this.addNewTask(this.texter.value);
+        handleNewTask(textValue = this.texter.value, isCompleted = false, fromLocalStorage = false) {
+            if (textValue) {
+                const task = this.createNewTask(textValue, isCompleted);
+                this.addNewTask(task, fromLocalStorage);
+                
                 this.texter.value = null;
             }       
         },
+
         addToUncompletedTasks(newTaskObject) {
-            const uncompletedTasks = this.allTasks.filter(
-                (task) => {
-                    return task.taskDiv.className === `task`;
-                }
-            );
-            let lastUncompletedTaskIndex;
-            let lastUncopletedTask;
-            if (uncompletedTasks.length) {
-                lastUncopletedTask = uncompletedTasks[uncompletedTasks.length - 1];
-                lastUncompletedTaskIndex = this.allTasks.lastIndexOf(lastUncopletedTask);
-            } else {
-                lastUncopletedTask = -1;
-            }
-            if (lastUncompletedTaskIndex > -1) {
-                lastUncopletedTask.taskDiv.after(newTaskObject.taskDiv);
+            const uncompletedTasks = this.allTasks.filter( task => task.taskDiv.className === 'task' );
+            const lastUncompletedTaskIndex = uncompletedTasks.length ? uncompletedTasks.length - 1 : -1;
+            const lastUncompletedTask = lastUncompletedTaskIndex !== -1 ? uncompletedTasks[lastUncompletedTaskIndex] : null;
+
+            if (lastUncompletedTaskIndex !== -1) {
+                lastUncompletedTask.taskDiv.after(newTaskObject.taskDiv);
                 this.allTasks.splice(lastUncompletedTaskIndex + 1, 0, newTaskObject);
-            }
-            else if (this.allTasks.length) {
-                this.allTasks[0].taskDiv.before(newTaskObject.taskDiv);
-                this.allTasks.splice(lastUncompletedTaskIndex, 0, newTaskObject);
-            }
-            else {
-                document.body.append(newTaskObject.taskDiv);
+                addTaskToLocalStorage(newTaskObject, lastUncompletedTaskIndex + 1);
+            } else {
+                if (this.allTasks.length) {
+                    this.allTasks[0].taskDiv.before(newTaskObject.taskDiv);
+                } else {
+                    document.body.append(newTaskObject.taskDiv);
+                }
+
                 this.allTasks.unshift(newTaskObject);
+                addTaskToLocalStorage(newTaskObject, 0);
             }
-            this.activeTaskIndex = -1;
         }
     }
 }
 
-const taskHandler = new TaskHandler(`newTask`, `addTask`);
-taskHandler.texter.addEventListener(`keyup`, 
-    (e) => {
-        if (e.keyCode === 13) {
-            taskHandler.handleNewTask();
-        }
-        if (e.keyCode === 39) {
-            e.target.nextSibling.focus();
-        }
-    }
-);
+const taskHandler = new TaskHandler('newTask', 'addTask');
 
-document.body.addEventListener(`keyup`,
+document.addEventListener('DOMContentLoaded', getLocalStorage(taskHandler) );
+
+document.body.addEventListener('keyup',
     (e) => {
-        const tasks = taskHandler.allTasks;   
-        if (e.key === `Enter`) {
-            if (taskHandler.activeTaskIndex != -1) {
-                let event = new Event(`click`);
-                let checkbox = tasks[taskHandler.activeTaskIndex].taskDiv.firstChild;
-                if (checkbox.checked) {
-                    checkbox.checked = false;
-                }
-                else {
-                    checkbox.checked = true;
-                }
-                checkbox.dispatchEvent(event);
+        const tasks = taskHandler.allTasks; 
+        const activeTaskIndex = tasks.findIndex( (task) => task.isOnFocus );
+        const activeTask = tasks[activeTaskIndex];
+        const hasActiveTask = activeTaskIndex !== -1;
+
+        if (hasActiveTask) {
+            const event = new Event('click');
+
+            switch (e.key) {
+                case 'Enter':
+                    const checkbox = activeTask.taskDiv.firstChild;
+
+                    checkbox.checked = !checkbox.checked;
+                    checkbox.dispatchEvent(event);
+
+                    break;
+                case 'Backspace':
+                    activeTask.taskDiv.lastChild.dispatchEvent(event);
+
+                    break;
             }
         }
-        if (e.key === ` `) {
-            if (taskHandler.activeTaskIndex != -1) {
-                tasks[taskHandler.activeTaskIndex].taskDiv.blur();
-            }
-            taskHandler.activeTaskIndex = -1;
+
+        if (e.key === ' ') {
             taskHandler.texter.focus();
         }
-        if (e.key === `Backspace`) {
-            if (taskHandler.activeTaskIndex !=-1) {
-                let event = new Event(`click`);
-                tasks[taskHandler.activeTaskIndex].taskDiv.lastChild.dispatchEvent(event);
+
+        if ( (e.key === 'ArrowDown' || e.key === 'ArrowUp') && tasks.length) {
+            if (e.key === 'ArrowDown') {
+                if (activeTaskIndex + 1 === tasks.length) {
+                    taskHandler.texter.focus();
+                } else {
+                    tasks[activeTaskIndex + 1].taskDiv.focus();
+                }
             }
-        }
-        if (e.key === `ArrowDown` || e.key === `ArrowUp`) {
-            if (tasks.length !=0) {
-                e.target.blur();
-                if (taskHandler.activeTaskIndex != -1) {
-                    tasks[taskHandler.activeTaskIndex].taskDiv.blur();
+
+            if (e.key === 'ArrowUp') {
+                switch (activeTaskIndex) {
+                    case -1:
+                        tasks[tasks.length - 1].taskDiv.focus();
+                        break;
+                    case 0: 
+                        taskHandler.texter.focus();
+                        break;
+                    default: 
+                        tasks[activeTaskIndex - 1].taskDiv.focus();
                 }
-                if (e.key === `ArrowDown`) {
-                    if (taskHandler.activeTaskIndex === -1) {
-                        ++taskHandler.activeTaskIndex;
-                    } else {
-                        if (taskHandler.activeTaskIndex + 2 <= tasks.length) {
-                            ++taskHandler.activeTaskIndex;
-                        } else {
-                            taskHandler.activeTaskIndex = 0;
-                        }
-                    }
-                }
-                if (e.key === `ArrowUp`) {
-                    if (taskHandler.activeTaskIndex === -1) {
-                        taskHandler.activeTaskIndex = tasks.length - 1;
-                    } else {
-                        if (taskHandler.activeTaskIndex === 0) {
-                            taskHandler.activeTaskIndex = tasks.length - 1;
-                        } else {
-                            --taskHandler.activeTaskIndex;
-                        }
-                    }x  
-                }
-                tasks[taskHandler.activeTaskIndex].taskDiv.focus();
             }
         }
     }
 );
 
-taskHandler.texter.addEventListener(`focus`, 
-    (e)=>{
-        if (taskHandler.activeTaskIndex != -1) {
-            if (e.target.parentNode != taskHandler.allTasks[taskHandler.activeTaskIndex]) {
-                taskHandler.allTasks[taskHandler.activeTaskIndex].taskDiv.blur();
-                taskHandler.activeTaskIndex = -1;
-            }
+taskHandler.texter.addEventListener('keyup', 
+    (e) => {
+        if (e.key === 'Enter') {
+            taskHandler.handleNewTask();
+        }
+
+        if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+            taskHandler.adder.focus();
+        }
+    }
+);  
+
+taskHandler.adder.addEventListener('keyup',
+    (e) => {
+        if (e.key === 'Enter') {
+            taskHandler.handleNewTask();
+        }
+
+        if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            taskHandler.texter.focus();
         }
     }
 );
-
-function getLocalStoradge(taskHandler) {
-    if (localStorage.length) {
-        for (let i = 0; i < localStorage.length; i++) {
-            const localStorageTask = JSON.parse( localStorage.getItem(i) );
-            taskHandler.readLocalStoradge(localStorageTask.textValue, localStorageTask.completed);
-        }
-    }
-}
-
-function setLocalStoradge(tasks) {
-    localStorage.clear();
-    let counter = -1
-    for (let task of tasks) {
-        counter++;
-        const localStorageTask = new Object();
-        localStorageTask.completed = task.isCompleted();
-        localStorageTask.textValue = task.getTextValue();
-        localStorage.setItem(counter, JSON.stringify(localStorageTask) );
-    }
-}
-
-document.addEventListener(`DOMContentLoaded`, getLocalStoradge(taskHandler) );
